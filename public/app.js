66af35617595f9921685a4c13c8bb589e872faa6
Jenny Lo <ajmoser@Jennys-MacBook-Air.local>
2017-09-10 13:43:17 -0700

    Added form to add a blog and added a delete button

--- public/app.js
+++ public/app.js
@@ -1,23 +1,48 @@
-(function() {
-
-  angular.module('blog_app', [])
-  .controller('mainController', ['$http', function($http) {
+  const app = angular.module('blog_app', []);
+  app.controller('mainController', ['$http', function($http) {
 
       this.message= "Hello from ANGULAR!"
       this.blogs = [];
-      this.herokuURL = 'https://gizmo-blogger-backend.herokuapp.com/blogs'
+      this.herokuURL = 'https://gizmo-blogger-backend.herokuapp.com/blogs';
+      this.formData = {};
 
-      $http({
-        method: 'GET',
-        url: 'http://localhost:3000/blogs'
-        // url: this.herokuURL
-      }).then(function(result) {
-          console.log('blogs from api: ', result);
-          this.blogs = result.data;
-      }.bind(this), function(error) {
-          console.log(error);
-      });
-
+      this.getBlogs = function (){
+        $http({
+          method: 'GET',
+          url: 'http://localhost:3000/blogs'
+          // url: this.herokuURL
+        }).then(function(result) {
+            console.log('blogs from api: ', result);
+            this.blogs = result.data;
+        }.bind(this), function(error) {
+            console.log(error);
+        });
+      }
+      this.processForm = () => {
+        console.log('process form is running');
+        console.log('here is the form data: ', this.formData);
+        $http({
+          method: 'POST',
+          url: 'http://localhost:3000/blogs',
+          data: this.formData
+        }).then(response => {
+          console.log(response);
+          this.blogs.unshift(response.data);
+        })
+        .catch(err=> console.log(err));
+      }
+      this.deleteBlog = function(blog){
+        $http({
+          method: 'DELETE',
+          url: 'http://localhost:3000/blogs' + blog._id
+        }).then(
+          function(response){
+            conroller.getBlogs();
+          },
+          function (error){
+            console.log(error);
+          }
+        );
+      }
+      this.getBlogs();
     }]);
-
-}());
